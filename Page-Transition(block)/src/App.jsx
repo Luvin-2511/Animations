d4d43f2df45d1d1811c526f375edc0d2861073f5
luvin25111-del <luvdaku2212@gmail.com>
2026-09-01 15:32:20 +0530

    App.jsx added

---
+++ Page-Transition(block)/src/App.jsx
@@ -0,0 +1,12 @@
+import PageTransition from "./animation/PageTransition"
+
+const App = () => {
+  return (
+    <main>
+      <PageTransition />
+      <h1>HELLO THERE</h1>
+    </main>
+  )
+}
+
+export default App

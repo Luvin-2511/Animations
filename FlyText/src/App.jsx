f7d30924506f08643a8d40379ce9232ebe0d984a
luvin25111-del <luvdaku2212@gmail.com>
2026-09-04 18:49:02 +0530

    App.jsx for text Animation

--- FlyText/src/App.jsx
+++ FlyText/src/App.jsx
@@ -1,10 +1,14 @@
-
+import Flyer from './components/Flyer'
+import Gap from './components/Gap'
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <main>
+      <Gap />
+      <Flyer text={"Fly out"} type='flyOut' ease={"elastic"}/>
+      <Flyer text={"Fly in"} type='flyIn' ease={"expo"}/>
+      <Gap />
+    </main>
   )
 }
 

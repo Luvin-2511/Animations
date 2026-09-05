63f26cd812b39f4fae8535b33f78adc2686658e6
luvin25111-del <luvdaku2212@gmail.com>
2026-09-05 18:54:15 +0530

    App.jsx modified for text effect

--- FlyText/src/App.jsx
+++ FlyText/src/App.jsx
@@ -5,8 +5,16 @@ const App = () => {
   return (
     <main>
       <Gap />
-      <Flyer text={"Fly out"} type='flyOut' ease={"elastic"}/>
-      <Flyer text={"Fly in"} type='flyIn' ease={"expo"}/>
+      <Flyer text={'Fly out'} type='flyOut' ease={'elastic'} />
+      <Flyer text={'Fly in'} type='flyIn' ease={'expo'} />
+      <Flyer
+        text={
+          'Paragraph on Kindness in 100 WordsBeing kind to others charges no money. It is a unique characteristic of showing love and caring for others selflessly. It is an act of making others happy without any specific reasons.'
+        }
+        para={true}
+        type='flyIn'
+        ease={'expo'}
+      />
       <Gap />
     </main>
   )

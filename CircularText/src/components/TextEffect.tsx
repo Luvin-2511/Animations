f88c83a5c0506309b03b957791afb561ceae9262
luvin25111-del <luvdaku2212@gmail.com>
2026-09-10 19:35:11 +0530

    TextEffect added

--- CircularText/src/components/TextEffect.tsx
+++ CircularText/src/components/TextEffect.tsx
@@ -1,14 +1,27 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
 type Props = {
-  text: "Animating";
+  text: string;
 };
 
 const TextEffect = (props: Props) => {
   const splitText: string[] = props.text.split("");
-  const radius = 100;
+  const radius = 300;
+  useGSAP(() => {
+    gsap.to(".text-wrapper", {
+      rotateY: -360,
+      rotateZ: -360,
+      rotateX: -360,
+      duration: 5,
+      ease: "none",
+      repeat: -1,
+    });
+  }, []);
   return (
     <div className="text-wrapper">
       {splitText.map((letter, idx) => {
-        const angle: number = (idx / (splitText.length - 1)) * Math.PI *2;
+        const angle: number = (idx / (splitText.length )) * Math.PI *2 ;
         return (
           <span
             style={{
@@ -16,7 +29,8 @@ const TextEffect = (props: Props) => {
                 ${Math.cos(angle) * radius}px,
                 0,
                 ${Math.sin(angle) * radius}px
-              ) `,
+              ) 
+                 rotateY(${Math.PI / 2 - angle}rad)`,
             }}
             className="rounded-letter"
           >

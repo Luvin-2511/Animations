b6aeccaabc7f8b23e99f8953ced77b26d046af1e
luvin25111-del <luvdaku2212@gmail.com>
2026-09-13 16:05:52 +0530

    App.jsx added for effevtcc

---
+++ Port-1/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Canvas } from '@react-three/fiber'
+import CanvasComponent from './components/CanvasComponent'
+
+const App = () => {
+  return (
+    <main>
+      <CanvasComponent />
+    </main>
+  )
+}
+
+export default App

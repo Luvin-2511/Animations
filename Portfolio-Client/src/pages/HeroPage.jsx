dd1db3941487e98f37698e80b36fd4a098ce4004
luvin25111-del <luvdaku2212@gmail.com>
2026-09-19 16:27:29 +0530

    Hero page added

---
+++ Portfolio-Client/src/pages/HeroPage.jsx
@@ -0,0 +1,15 @@
+import SvgText from '../components/SvgText'
+
+const HeroPage = () => {
+  return (
+    <>
+      <section className='hero-section'>
+        <div className='hero-name'>
+          <SvgText text='Sanyuct' fontSize={12} fontWeight={100}/>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default HeroPage
